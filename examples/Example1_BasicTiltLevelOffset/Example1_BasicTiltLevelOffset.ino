da0e9334c8c48900e36551e742a2c9ef6f95d92e
ValentinSiderskiyPhD <56981028+ValentinSiderskiyPhD@users.noreply.github.com>
2024-10-25 15:37:08 +1100

    Example 1 now works for W5500-EVB-Pico

--- examples/Example1_BasicTiltLevelOffset/Example1_BasicTiltLevelOffset.ino
+++ examples/Example1_BasicTiltLevelOffset/Example1_BasicTiltLevelOffset.ino
@@ -6,20 +6,34 @@
 #include <SPI.h>
 #include <SCL3300.h>
 
+#define SCL3300_SCK 14    // (Purple)
+#define SCL3300_MISO 12   // (Blue)
+#define SCL3300_MOSI 15   // (Yellow)
+#define SCL3300_CS 13     // (Brown)
+
 SCL3300 inclinometer;
 //Default SPI chip/slave select pin is D10
 
-// Need the following define for SAMD processors
-#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
-  #define Serial SERIAL_PORT_USBVIRTUAL
-#endif
+//// Need the following define for SAMD processors
+//#if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
+//  #define Serial SERIAL_PORT_USBVIRTUAL
+//#endif
 
 void setup() {
   Serial.begin(9600);
   delay(2000); //SAMD boards may need a long time to init SerialUSB
   Serial.println("Reading basic Tilt Level Offset values from SCL3300 Inclinometer");
 
-  if (inclinometer.begin() == false) {
+    // Initialize the SPI and SCL3300 sensor
+
+  SPI1.setSCK(SCL3300_SCK);
+  SPI1.setCS(SCL3300_CS);
+  SPI1.setRX(SCL3300_MISO);
+  SPI1.setTX(SCL3300_MOSI);
+//  
+//  
+
+  if (inclinometer.begin(SPI1,SCL3300_CS) == false) {
     Serial.println("Murata SCL3300 inclinometer not connected.");
     while(1); //Freeze
   }
@@ -27,13 +41,13 @@ void setup() {
 
 void loop() {
   if (inclinometer.available()) { //Get next block of data from sensor
-    Serial.print("X Tilt: ");
+    //Serial.print("X Tilt: ");
     Serial.print(inclinometer.getTiltLevelOffsetAngleX());
     Serial.print("\t");
-    Serial.print("Y Tilt: ");
+    //Serial.print("Y Tilt: ");
     Serial.print(inclinometer.getTiltLevelOffsetAngleY());
     Serial.print("\t");
-    Serial.print("Z Tilt: ");
+    //Serial.print("Z Tilt: ");
     Serial.println(inclinometer.getTiltLevelOffsetAngleZ());
     delay(100); //Allow a little time to see the output
   } else inclinometer.reset();
